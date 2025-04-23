f338eccd9772227ff4e9a2df9990738840c869e7
Leonel Abud <abudleito@gmail.com>
2025-04-23 15:39:09 -0300

    Initial commit

---
+++ Assets/Scripts/TP_UI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TP_UI : MonoBehaviour
+{
+    public TMP_InputField inputClave;
+    public TextMeshProUGUI resultado;
+    string clave = "clave";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultado.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ComprobarClaveIngresada()
+    {
+        string claveIngresada = inputClave.text;
+        if ( claveIngresada != clave)
+        {
+            resultado.text = "Acceso Denegado";
+            inputClave.text = "";
+        }
+        else
+        {
+            resultado.text = "Acceso garantizado";
+        }
+    }
+}
